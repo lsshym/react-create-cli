f55b54228eafe9ad5fc83bfea398a11aaefbe0bc
qiuyanli <qiuyanli@faceunity.com>
2023-05-29 16:37:23 +0800

    修改文档

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # react-create-generator
-妈的名字被占用了
-好名都被狗取了
+
+react 环境脚手架
+
 ### 使用说明
+
 react-create-generator create <project-name>
 rcc create <project-name>
--- bin/index.mjs
+++ bin/index.mjs
@@ -51,29 +51,3 @@ program
         );
     });
 program.parse(process.argv);
-
-// 自定义文本信息
-// const message = 'Loading unicorns'
-// // 初始化
-// const spinner = ora(message);
-// // 开始加载动画
-// spinner.start();
-
-// setTimeout(() => {
-//     // 修改动画样式
-
-//     // Type: string
-//     // Default: 'cyan'
-//     // Values: 'black' | 'red' | 'green' | 'yellow' | 'blue' | 'magenta' | 'cyan' | 'white' | 'gray'
-//     spinner.color = 'red';
-//     spinner.text = 'Loading rainbows';
-
-//     setTimeout(() => {
-//         // 加载状态修改
-//         spinner.stop() // 停止
-//         spinner.succeed('Loading succeed'); // 成功 ✔
-//         // spinner.fail(text?);  失败 ✖
-//         // spinner.warn(text?);  提示 ⚠
-//         // spinner.info(text?);  信息 ℹ
-//     }, 2000);
-// }, 2000);
